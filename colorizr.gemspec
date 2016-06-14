ef5c54dac7853a665669293c094e43e6f14c99bf
Tianqing <442034859@qq.com>
2016-06-14 20:07:23 +0800

    initial commit

---
+++ colorizr.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'colorizr'
+  s.version     = '0.0.1'
+  s.date        = '2016-06-14'
+  s.summary     = "Colors Everywhere!"
+  s.description = "A simple gem to colorize terminal output"
+  s.authors     = ["Tianqing Peng"]
+  s.email       = '442034859@qq.com'
+  s.files       = ["lib/colorizr.rb"]
+  s.homepage    =
+    'N/A'
+  s.license       = 'MIT'
+end
---
+++ lib/colorizr.rb
@@ -0,0 +1,24 @@
+class String
+  @@Colorizr_colors = {
+    :red => 31,
+    :green => 32,
+    :yello => 33,
+    :blue => 34,
+    :pink => 35,
+    :light_blue => 94,
+    :white => 97,
+    :light_grey => 37,
+    :black => 30
+  }
+
+  def self.create_colors
+    @@Colorizr_colors.each { |key,value|
+      class_eval{
+        def key
+             "\e[#{value}m" + self.to_s + "\e[0m"
+        end
+      }
+      }
+  end
+
+end
