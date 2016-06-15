50414bfa3593ba734bea3eb3f4a897aae574a586
Tianqing <442034859@qq.com>
2016-06-15 13:23:12 +0800

    all function done

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -14,9 +14,29 @@ class String
   def self.create_colors
     @@Colorizr_colors.each { |key,value|
       send(:define_method, "#{key}") do
-        "\e[#{value}m" + self.to_s + "\e[0m"
+        "\e[#{value}m" + self + "\e[0m" #self.to_s
       end
       }
   end
 
+  def self.colors
+      @@Colorizr_colors.map {|key, value| key}
+  end
+
+  def self.sample_colors
+      color_collection = colors #self.color
+      color_collection.each do |color| #color.to_s
+        # "This is #{color}"."#{color}", won't work
+        # things inside "" will be considered as String literal
+        sample = "This is #{color}"
+        puts sample.send(:"#{color}")
+
+      end
+
+  end
+
 end
+
+String.create_colors
+# String.colors
+# String.sample_colors
